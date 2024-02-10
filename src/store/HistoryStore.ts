816ad6ecfc3e0245015d6f754f035b7a88201b0b
Ruslan Aksenov <akseonowww@ya.ru>
2024-02-10 20:06:00 +0300

    Fix and add type for parameter addExpenses

--- src/store/HistoryStore.ts
+++ src/store/HistoryStore.ts
@@ -1,71 +1,72 @@
-import { makeAutoObservable } from 'mobx';
+import { makeAutoObservable } from "mobx";
 
 export interface Expense {
-	id: number;
-	date?: any;
-	cash: number;
-	category: number;
-	desc?: string;
+   id?: number;
+   date: string;
+   cash: number;
+   category: number;
+   desc?: string;
 }
 
 class HistoryStore {
-	expenses: Expense[] = [
-		{
-			id: 1,
-			date: '20230704T120854',
-			cash: 8000,
-			category: 1,
-		},
-		{
-			id: 2,
-			cash: 8000,
-			date: '20230704T120854',
-			category: 2,
-		},
-		{
-			id: 3,
-			cash: 8000,
-			date: '20230702T120854',
-			category: 1,
-		},
-		{
-			id: 4,
-			cash: 8000,
-			date: '20230704T120854',
-			category: 3,
-		},
-		{
-			id: 5,
-			cash: 8000,
-			date: '20230704T120854',
-			category: 1,
-		},
-		{
-			id: 6,
-			cash: 8000,
-			date: '20230702T120854',
-			category: 2,
-		},
-	];
+   expenses: Expense[] = [
+      {
+         id: 1,
+         cash: 8000,
+         desc: "кукукук",
+         date: "20230704T120854",
+         category: 1,
+      },
+      {
+         id: 2,
+         cash: 8000,
+         date: "20230704T120854",
+         category: 2,
+      },
+      {
+         id: 3,
+         cash: 8000,
+         date: "20230704T120854",
+         category: 1,
+      },
+      {
+         id: 4,
+         cash: 8000,
+         desc: "кукукук",
+         date: "20230704T120854",
+         category: 3,
+      },
+      {
+         id: 5,
+         cash: 8000,
+         date: "20230704T120854",
+         category: 1,
+      },
+      {
+         id: 6,
+         cash: 8000,
+         date: "20230704T120854",
+         category: 2,
+      },
+   ];
 
-	constructor() {
-		makeAutoObservable(this);
-	}
+   constructor() {
+      makeAutoObservable(this);
+   }
 
-	addExpenses(data: any) {
-		this.expenses.push({
-			id: this.expenses.length + 1,
-			date: data.date,
-			cash: data.cash,
-			category: data.category,
-			desc: data.desc,
-		});
-	}
+   addExpenses(data: Expense) {
+      this.expenses.push({
+         id: this.expenses.length + 1,
+         date: data.date,
+         cash: data.cash,
+         category: data.category,
+         desc: data.desc,
+      });
+   }
 
-	get getExpenses() {
-		console.log(this.expenses);
-		return this.expenses;
-	}
+   get getExpenses() {
+      return this.expenses;
+   }
 }
 
 const historyStore = new HistoryStore();
